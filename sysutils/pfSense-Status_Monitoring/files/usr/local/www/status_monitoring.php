5f9ffaf076d1ecee156d87f152347498c5118034
NOYB <Al_Stu@Frontier.com>
2016-12-22 22:34:12 -0800

    Status / Monitoring - Quick Links II - Genesis
    Quick links provides convenient one click direct access to graphs without the need to reload the entire page or find the desired graph in the saved views list.
    
    1) More convenient and quicker than expanding the saved views drop down and searching through the list for the desired graph.
    
    2) Refreshes only the graph.  This is faster and more efficient than reloading the entire page by selecting a saved view.
    
    This can be useful for quick convenient access to temporary graphs being used for issue investigation and troubleshooting as well as frequently accessed permanent graphs.

--- sysutils/pfSense-Status_Monitoring/files/usr/local/www/status_monitoring.php
+++ sysutils/pfSense-Status_Monitoring/files/usr/local/www/status_monitoring.php
@@ -64,6 +64,22 @@ function createSlug($string) {
 }
 
 $changedesc = gettext("Status: Monitoring:") . " ";
+
+if ($_POST && $_POST['quicklinks']) {
+
+	$quicklinks_string = preg_replace('/\*c\^o\#m\?m\@a\*$/', '', $_POST['quicklinks']);
+	$quicklinks_strings = explode('*c^o#m?m@a*', $quicklinks_string);
+
+	foreach ($quicklinks_strings as $quicklink_string) {
+		$quicklink = explode('*c^o#l?o@n*', $quicklink_string);
+		$quicklinks[$quicklink[0]] = array('title' => $quicklink[1], 'category' => $quicklink[2]);
+	}
+
+	$config['rrd']['quicklinks'] = $quicklinks;
+
+	write_config($desc = gettext("Status Monitoring Quick Links Saved (backup, no sync): "), $backup = true, $write_config_only = true);
+}
+
 if($_POST['enable']) {
 	if(($_POST['enable'] === 'false')) {
 		unset($config['rrd']['enable']);
@@ -199,6 +215,16 @@ if ($_POST['remove-view']) {
 
 }
 
+////save new defaults
+//if ($_POST['defaults']) {
+//	// Time period setting change applies valid resolutions and selects the default resolution of the time period.
+//	// Time period setting needs to be before resolution so that the time period default doesn't override the page load default resolution.
+//	// Time period setting needs to be before custom start/end date/time so those will be enabled before applying their setting.
+//	$config['rrd']['category'] = "left=".$_POST['graph-left']."&right=".$_POST['graph-right']."&timePeriod=".$_POST['time-period']."&resolution=".$_POST['resolution']."&startDate=".$_POST['start-date']."&endDate=".$_POST['end-date']."&startTime=".$_POST['start-time']."&endTime=".$_POST['end-time']."&graphtype=".$_POST['graph-type']."&invert=".$_POST['invert']."&refresh-interval=".$_POST['refresh-interval'];
+//	write_config(gettext("Status Monitoring Default Saved"));
+//	$savemsg = "The changes have been applied successfully.";
+//}
+
 $pconfig['enable'] = isset($config['rrd']['enable']);
 
 if(isset($_GET['view'])) {
@@ -236,6 +262,14 @@ if (is_array($config['rrd']['savedviews'])) {
 
 	$pconfig['category'] = $config['rrd']['category'];
 
+}
+
+// Load specified quick link or default graph
+if ($_POST && $_POST['selected_quicklink']) {
+	$selected_quicklink = $_POST['selected_quicklink'];
+	$pconfig['category'] = $config['rrd']['quicklinks'][$selected_quicklink]['category'];
+//} else {
+//	$pconfig['category'] = $config['rrd']['category'];
 }
 
 $system = $packets = $quality = $traffic = $captiveportal = $ntpd = $queues = $queuedrops = $dhcpd = $vpnusers = $wireless = $cellular = [];
@@ -658,6 +692,7 @@ display_top_tabs($tab_array);
 					<button class="btn btn-sm btn-primary" type="submit" value="true" name="save-view" id="save-view" style="display:none;"><i class="fa fa-save fa-lg"></i> Save View</button>
 					<button class="btn btn-sm btn-primary" type="button" value="true" name="add-view" id="add-view" style="display:none;"><i class="fa fa-plus fa-lg"></i> Add View</button>
 					<button class="btn btn-sm btn-primary" type="submit" value="true" name="remove-view" id="remove-view" style="display:none;"><i class="fa fa-trash fa-lg"></i> Remove View</button>
+<!--					<button class="btn btn-sm btn-primary" type="submit" value="true" name="defaults" id="defaults" style="display:none;"><i class="fa fa-save fa-lg"></i> Save As Defaults</button>-->
 				</div>
 				<div class="col-sm-4">
 					<?php
@@ -685,10 +720,67 @@ display_top_tabs($tab_array);
 
 <div class="panel panel-default">
 	<div class="panel-heading">
-		<h2 class="panel-title">Interactive Graph</h2>
+		<h2 class="panel-title" style="display:inline-block">
+			Interactive Graph
+		</h2>
+		<span class="fa fa-spinner fa-pulse fa-lg fa-fw" id="loading-msg"></span>
+<?php
+		if (is_array($config['rrd']['quicklinks'])) {
+			$quicklinks_visibility = "visible";
+		} else {
+			$quicklinks_visibility = "invisible";
+		}
+?>
+		<ul class="pull-right context-links list-inline" style="padding-left:16px">
+			<li>
+				<a id="quicklinks_save" class="invisible" >
+					<i class="fa fa-save icon-pointer" title="Save the Quick Links Configuration"></i>
+				</a>
+			</li>
+			<li>
+				<a id="quicklink_delete" class="<?=$quicklinks_visibility?>" >
+					<i class="fa fa-trash icon-pointer" title="Delete the Selected Quick Link"></i>
+				</a>
+			</li>
+			<li>
+				<a id="quicklink_add">
+					<i class="fa fa-plus icon-pointer" title="Add Quick Link for Current Graph"></i>
+				</a>
+			</li>
+		</ul>
+
+		<ul id="quicklinks_list" class="pull-right context-links list-inline" style="padding-left:16px">
+<?php
+		if (is_array($config['rrd']['quicklinks'])) {
+			foreach ($config['rrd']['quicklinks'] as $key => $quicklink) {
+
+				$graphOptions_strings = explode('&', $quicklink['category']);
+
+				foreach ($graphOptions_strings as $graphOption_string) {
+					$graphOption = explode('=', $graphOption_string);
+					$graphOptions[$graphOption[0]] = $graphOption[1];
+				}
+
+				$quicklink_icon = 'fa-' . $graphOptions['graphtype'] . '-chart';
+?>
+			<li>
+				<a id="<?=$key?>" title="<?=$quicklink['title']?>" data-category="<?=$quicklink['category']?>" >
+					<i class="fa <?=$quicklink_icon?> icon-pointer"></i>
+				</a>
+			</li>
+<?php
+			}
+		}
+?>
+		</ul>
+
+		<ul class="pull-right context-links list-inline" style="padding-left:16px">
+			<li>
+				<a id="quicklinktitle"></a>
+			</li>
+		</ul>
 	</div>
 	<div class="panel-body">
-		<div class="alert alert-info" id="loading-msg">Loading Graph...</div>
 		<div id="chart-error" class="alert alert-danger" style="display: none;"></div>
 		<div id="monitoring-chart" class="d3-chart">
 			<svg id="monitoring-svg"></svg>
@@ -729,9 +821,31 @@ display_top_tabs($tab_array);
 	</div>
 </div>
 
+<div class="hidden" id="quicklinksSequence">
+	<form action="/status_monitoring.php" method="post" id="quicklinksSequence_form" name="quicklinksForm">
+		<input type="hidden" name="quicklinks" value="" />
+		<input type="hidden" name="selected_quicklink" value="" />
+	</form>
+</div>
+
 <script type="text/javascript">
 
 //<![CDATA[
+
+function updateQuicklinks() {
+	var quicklinks = '';
+
+	$( "#quicklinks_list [id^=quicklink]" ).each(function(idx, quicklink) {
+		quicklinks += quicklink.id + '*c^o#l?o@n*' + quicklink.title + '*c^o#l?o@n*' + quicklink.dataset.category + '*c^o#m?m@a*';
+
+		if ($(quicklink).prop('selected')) {
+			$('input[name=selected_quicklink]', $('#quicklinksSequence_form')).val(quicklink.id);
+		}
+	});
+
+	$('input[name=quicklinks]', $('#quicklinksSequence_form')).val(quicklinks);
+}
+
 events.push(function() {
 
 	//lookup axis labels based on graph name
@@ -972,6 +1086,8 @@ events.push(function() {
 	***/
 
 	//TODO work in more validation
+	var settings_previous = false;
+
 	function getOptions() {
 		var error = "There was an error getting the options.";
 
@@ -1005,6 +1121,14 @@ events.push(function() {
 		}
 
 		var graphOptions = 'left=' + graphLeft + '&right=' + graphRight + '&start=' + start + '&end=' + end + '&resolution=' + resolution + '&timePeriod=' + timePeriod + '&graphtype=' + graphtype + '&invert=' + invert + '&refreshInterval=' + refreshInterval;
+
+		var settings_current = 'left=' + graphLeft + '&right=' + graphRight + '&timePeriod=' + timePeriod + '&resolution=' + resolution + '&startDate=' + startDate + '&endDate=' + endDate + '&startTime=' + startTime + '&endTime=' + endTime + '&graphtype=' + graphtype + '&invert=' + invert + '&refreshInterval=' + refreshInterval;
+
+		// If graph settings have changed, un-select any quick link.
+		if ((settings_current != settings_previous) && settings_previous) {
+			selected_quicklink(null);
+		}
+		settings_previous = settings_current;
 
 		return graphOptions;
 	}
@@ -1190,7 +1314,6 @@ events.push(function() {
 	});
 
 	$( ".update-graph" ).click(function() {
-		$("#monitoring-chart").hide();
 		$("#loading-msg").show();
 		$("#chart-error").hide();
 		draw_graph(getOptions());
@@ -1201,6 +1324,7 @@ events.push(function() {
 		$("#save-view").toggle();
 		$("#add-view").toggle();
 		$("#remove-view").toggle();
+//		$("#defaults").toggle();
 		$("#enable").toggle();
 		$("#ResetRRD").toggle();
 	});
@@ -1566,6 +1690,154 @@ events.push(function() {
 		$('acronym').tooltip();
 	}
 
+	// Refresh graph scheduler.
+	var refresh_id;
+	function refresh_graph_scheduler() {
+		var refresh_interval = $( "#refresh-interval" ).val();
+		Visibility.stop(refresh_id);
+		if (refresh_interval > 0) {
+			refresh_id = Visibility.every(refresh_interval, function () {
+				$("#chart-error").hide();
+				draw_graph(getOptions());
+			});
+		}
+	}
+
+	// Load graph of clicked quick link.
+	// Using long form .on() delegated event here because click event is not bound to dynamically added elements (quick links).  Thus the short form .click() does not work.
+	$( '#quicklinks_list' ).on( "click", "[id^=quicklink]", function() {
+		if ($(this).attr('data-category')) {
+			$("#loading-msg").show();
+			applySettings($(this).attr('data-category').trim());
+			$("#chart-error").hide();
+			draw_graph(getOptions());
+			refresh_graph_scheduler();
+			selected_quicklink(this);
+		}
+	});
+
+	// Make quick links sortable.
+	$('#quicklinks_list').sortable({
+		cursor: 'grabbing',
+		update: function(event, ui) {
+			reindex_quicklinks(ui.item.parent('ul'));
+		}
+	});
+
+	// Add quick link for the current graph settings.
+	$('#quicklink_add').click(function () {
+		title = prompt("Enter a title for the quick link.", "");
+		if (title == null || title == "") { return undefined; }
+
+		$( "#selectedquicklinktitle" ).text(title);
+
+		id = 'quicklink999';
+
+		// Time period setting change applies valid resolutions and selects the default resolution of the time period.
+		// Time period setting needs to be before resolution so that the time period default doesn't override the page load default resolution.
+		// Time period setting needs to be before custom start/end date/time so those will be enabled before applying their setting.
+		graph_settings  = 'left=' + (($( "#graph-left" ).val()) ? $( "#graph-left" ).val() : '');
+		graph_settings += '&right=' + (($( "#graph-right" ).val()) ? $( "#graph-right" ).val() : '');
+		graph_settings += '&timePeriod=' + $( "#time-period" ).val();
+		graph_settings += '&resolution=' + $( "#resolution" ).val();
+		graph_settings += '&startDate=' + $( "#start-date" ).val();
+		graph_settings += '&endDate=' + $( "#end-date" ).val();
+		graph_settings += '&startTime=' + $( "#start-time" ).val();
+		graph_settings += '&endTime=' + $( "#end-time" ).val();
+		graph_settings += '&graphtype=' + $( "#graph-type" ).val();
+		graph_settings += '&invert=' + $( "#invert" ).val();
+		graph_settings += '&refresh-interval=' + $( "#refresh-interval" ).val();
+
+		quicklink_icon = 'fa-' + $( "#graph-type" ).val() + '-chart';
+
+		quicklink_list_item_html = '<li class="ui-sortable-handle"><a id="' + id + '" title="' + title + '" data-category="' + graph_settings + '" ><i class="fa ' + quicklink_icon + ' icon-pointer"></i></a></li>';
+
+		$(quicklink_list_item_html)
+		.appendTo($( "#quicklinks_list" ));
+
+		_this = $( "#quicklinks_list #" + id );
+		selected_quicklink(_this);
+
+		reindex_quicklinks($( "#quicklinks_list" ));
+	});
+
+	// Delete the selected quick link from the quick links list.
+	$('#quicklink_delete').click(function () {
+		$( "#quicklinks_list [id^=quicklink]" ).each(function() {
+			if ($(this).prop('selected')) {
+				$(this).parent().remove('li');
+				$( "#quicklinktitle" ).text("");
+				reindex_quicklinks($( "#quicklinks_list" ));
+			}
+		});
+	});
+
+	// Save quick links changes to the config, clear dirty flag, and suppress save icon.
+	$( "#quicklinks_save" ).click(function() {
+		updateQuicklinks();
+		dirty = false;
+		$( "#quicklinks_save" ).removeClass("visible");
+		$( "#quicklinks_save" ).addClass("invisible");
+		$('[name=quicklinksForm]').submit();
+	});
+
+	// Provide a warning message if the user tries to leave the page with unsaved changes.
+	$(window).bind('beforeunload', function(){
+		if (dirty) {
+			return ("<?=gettext('One or more quick links have been changed but have not yet been saved.')?>");
+		} else {
+			return undefined;
+		}
+	});
+
+	// Style the selected and unselected quick links.
+	function selected_quicklink(selected) {
+		unselectedquicklinkcolor = 'silver';
+		selectedquicklinkcolor = $( "#quicklink_add" ).css("color");
+
+		$( "#quicklinks_list [id^=quicklink]" ).css("color", unselectedquicklinkcolor);
+		$(selected).css("color", selectedquicklinkcolor);
+
+		$( "#quicklinks_list [id^=quicklink]" ).prop('selected', false);
+		$(selected).prop('selected', true);
+
+		$( "#quicklinktitle" ).text('');
+		$( "#quicklinktitle" ).text($(selected).attr("title"));
+	}
+
+	// Do on page load to set initial quick links styling.
+	if ("<?=$selected_quicklink?>") {	// Returning to the previous selected quick link (ex: after save).
+		_this = $( "#quicklinks_list #<?=$selected_quicklink?>" );
+		_this.prop('selected', true);
+		selected_quicklink(_this);
+	} else {							// Before any quick link is selected.
+		selected_quicklink(null);
+	}
+
+	// Re-index the quick links order and mark dirty.
+	function reindex_quicklinks(section) {
+		var row = 0;
+
+		// Quick links may have all been deleted.  So conceal the delete icon until at least one quick link is detected.
+		$( "#quicklink_delete").removeClass("visible");
+		$( "#quicklink_delete").addClass("invisible");
+
+		section.find('a').each(function() {
+			if(this.id) {
+				$(this).attr("id", "quicklink" + row);
+				row++;
+
+				// Quick links exist (detected).  So display the delete icon.
+				$( "#quicklink_delete").removeClass("invisible");
+				$( "#quicklink_delete").addClass("visible");
+			}
+		});
+		// Quick links changes have been made.  So display the save icon.
+		$( "#quicklinks_save" ).removeClass("invisible");
+		$( "#quicklinks_save" ).addClass("visible");
+		dirty = true;
+	}
+
 	var chart;
 
 	<?php
@@ -1585,16 +1857,7 @@ events.push(function() {
 	} else {
 
 		draw_graph(getOptions());
-
-		var refresh_interval = $( "#refresh-interval" ).val();
-
-		if(refresh_interval > 0) {
-
-			var refresh_id = Visibility.every(refresh_interval, function () {
-			    draw_graph(getOptions());
-			});
-
-		}
+		refresh_graph_scheduler();
 
 	}
 
